f10aaad52e47fa9b3f8c4601d1571d5b30eb9cea
DaNJO-Y <122925211+DaNJO-Y@users.noreply.github.com>
2025-03-31 17:11:27 -0400

    Added Abstract powerups class and extended powerups based on those classes

---
+++ src/YellowBoltPowerUp.java
@@ -0,0 +1,20 @@
+import java.awt.Graphics2D;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Rectangle2D;
+import java.awt.geom.Line2D;
+import javax.swing.JPanel;
+import java.util.Random;
+import java.awt.Image;
+import java.util.ArrayList;
+import java.awt.image.BufferedImage;
+public class YellowBoltPowerUp extends PowerUp {
+    
+    public YellowBoltPowerUp(String effect, float duration) {
+        super(effect, duration);
+
+    }
+
+    public void applyEffect() {
+        //implement function;
+    }
+}
