90f4137c3064a705928ac1288e8806073d3abeef
ErrorUnknown55 <147456278+ErrorUnknown55@users.noreply.github.com>
2025-04-01 19:31:17 -0400

    Ajusted

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
+import javax.swing.JFrame;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        JFrame gameWindow =  new GameWindow();
     }
 }
