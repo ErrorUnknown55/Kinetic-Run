b773069b9f46d7bdcc568ea4d6ade5a15e252e53
ErrorUnknown55 <147456278+ErrorUnknown55@users.noreply.github.com>
2025-04-02 23:37:40 -0400

    Added background Color and title

--- src/GameWindow.java
+++ src/GameWindow.java
@@ -1,3 +1,9 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.GradientPaint;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import javax.swing.*;
 
 public class GameWindow extends JFrame {
@@ -8,8 +14,30 @@ public class GameWindow extends JFrame {
         
         //Set Panel Size
         setSize(700,650);
-        
-        setLocationRelativeTo(null);
+
+        //Creates a custom background for the mainPanel
+        JPanel mainPanel =  new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                Graphics2D g2d = (Graphics2D) g;
+                Color color1 = new Color(20,30,48);
+                Color color2 = new Color(36, 59, 85);
+                g2d.setPaint(new GradientPaint(0,0, color1, getWidth(), getHeight(), color2));
+                g2d.fillRect(0, 0, getWidth(), getHeight());
+            }
+        };
+
+        mainPanel.setLayout(new BorderLayout());
+        add(mainPanel);
+
+        JLabel titleJLabel =  new JLabel("KINETIC RUN", SwingConstants.CENTER);
+        titleJLabel.setFont(new Font("Arial", Font.BOLD, 48));
+        titleJLabel.setForeground(Color.WHITE);
+        titleJLabel.setBorder(BorderFactory.createEmptyBorder(50, 0, 100, 0));
+        mainPanel.add(titleJLabel, BorderLayout.NORTH);
+
+        setLocationRelativeTo(null);//Center the window
         setResizable(false);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
