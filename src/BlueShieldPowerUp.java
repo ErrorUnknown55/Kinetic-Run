b440b60ddf3172cd43ed6b20b5bcbf28aa95dca8
DaNJO-Y <122925211+DaNJO-Y@users.noreply.github.com>
2025-04-12 13:26:12 -0400

    Attempt at readding of Power Up classes

---
+++ src/BlueShieldPowerUp.java
@@ -0,0 +1,22 @@
+import java.awt.Graphics2D;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Rectangle2D;
+import java.awt.geom.Line2D;
+import javax.swing.JPanel;
+import java.util.Random;
+import java.awt.Image;
+import java.util.ArrayList;
+import java.awt.image.BufferedImage;
+public class BlueShieldPowerUp extends PowerUp{
+    
+    
+    public BlueShieldPowerUp(String effect, float duration) {
+        super(effect, duration);
+        
+    }
+
+    @Override
+    public void applyEffect() {
+        //implement function;
+    }
+}
