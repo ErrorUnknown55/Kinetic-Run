cf3b5e12613a81e0c5527f2332f4ad7326a65a06
ErrorUnknown55 <147456278+ErrorUnknown55@users.noreply.github.com>
2025-04-24 10:04:37 -0400

    Player interaction with platform and jump

--- src/Player.java
+++ src/Player.java
@@ -1,113 +1,3 @@
-// import java.awt.Graphics;
-// import java.awt.Image;
-// import java.awt.geom.Rectangle2D;
-
-// public class Player {
-    
-//     //Veriables
-//     private int pX, pY; //Player position
-//     private int pWidth, pHeight; // Size of player
-//     private int pSpeed; // Speed of the player
-
-//     //Player Animation Sequence
-//     private Image[] walkImages =  new Image[5];
-//     private Image[] swimImages = new Image[2];
-//     private Image[] jumpImages =  new Image[3];
-
-//     //Player Images
-//     private Image deadImage, duckImage, fallImage, hitImage, rollImage, standImage;
-
-//     public Player(int x,  int y) {
-//         //Player position
-//         this.pX = x;
-//         this.pY = y;
-
-//         //Load images
-//         deadImage = ImageManager.loadImage("images/player/PlayerBlue/playerBlue_dead.png");
-//         duckImage = ImageManager.loadImage("images/player/PlayerBlue/playerBlue_duck.png");
-//         fallImage = ImageManager.loadImage("images/player/PlayerBlue/playerBlue_fall.png");
-//         hitImage = ImageManager.loadImage("images/player/PlayerBlue/playerBlue_hit.png");
-//         standImage = ImageManager.loadImage("images/player/PlayerBlue/playerBlue_stand.png");
-
-//         //Laod Player Walk Animation Sequence
-//         for(int i = 0; i < walkImages.length; i++)
-//             walkImages[i] = ImageManager.loadImage("images/player/PlayerBlue/playerBlue_walk"+ (i+1)+".png");
-
-//         //Load Player Jump Animation Sequence
-//         for(int i = 0;  i < jumpImages.length; i++)
-//             jumpImages[i] = ImageManager.loadImage("images/player/PlayerBlue/playerBlue_jump"+ (i+1)+".png");
-
-//         //Load player Swim Animation Sequence
-//         for(int i = 0; i < swimImages.length; i++)
-//             swimImages[i] = ImageManager.loadImage("images/player/PlayerBlue/playerBlue_swim1"+ (i+1)+".png");
-//     }
-
-
-//     public int getX() {
-//         return pX;
-//     }
-
-//     public int getY() {
-//         return pY;
-//     }
-
-//     public int getWidth() {
-//         return pWidth;
-//     }
-
-//     public int getHeight() {
-//         return pHeight;
-//     }
-
-//     // Draw the player
-//     public void draw(Graphics g2d) {
-//         g2d.drawImage(standImage, pX, pY, pWidth, pHeight, null);
-//     }
-
-//     //Control the player movement
-//     public void movement(int dir) {
-        
-//         if(dir == 8) {//Player jump
-//             pY -= pSpeed;
-//             // Prevents the player from moving out of the screen
-//             if (pY < 0)
-//                 pY = 0;
-//         } else if(dir == 5 ) {//Player Duck
-//             pY += pSpeed;
-//             // Prevents the player from moving out of the screen
-//             if(pY > (400 - pHeight))
-//                 pY = 400 - pHeight;
-
-//         } else if(dir == 4){//Move the player Left
-//             pX -= pSpeed;
-//             //Prevents the player from moving out of the screen
-//             if (pX < 0)
-//                  pX = 0;
-//         } else if(dir == 6){// Move the player Right
-//             pX += pSpeed;
-//             // Prevents the player from moving out of the screen
-//             if (pX > (400 - pWidth))
-//                 pX = 400 - pWidth;
-//         }
-
-//     }
-
-//     //
-//     public void jump() {
-
-//     }
-
-//     public boolean isOnPlayer(int x, int y) {
-//         Rectangle2D.Double myRectangle = getBoundingRectangle();
-//         return myRectangle.contains(x, y);
-//     }
-
-//     public Rectangle2D.Double getBoundingRectangle() {
-//         return new Rectangle2D.Double(pX, pY, pWidth, pHeight);
-//     }
-
-
-// }
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.geom.Rectangle2D;
@@ -121,6 +11,12 @@ public class Player {
     private int currentDirection = 0; // To track current movement direction
     private int walkFrame = 0; // For walk animation
     private long lastWalkFrameTime = 0; // For animation timing
+    
+    private boolean isJumping = false;
+    private boolean isOnGround = false;
+    private float gravity = 0.5f;
+    private float verticalVelocity = 0;
+    private float jumpForce = -12f;
 
     // Player Animation Sequence
     private Image[] walkImages = new Image[5];
@@ -173,6 +69,21 @@ public class Player {
         return pHeight;
     }
 
+    public void update() {
+        //Apply Gravity if not on the ground
+        if(!isOnGround) {
+            verticalVelocity += gravity;
+            pY += verticalVelocity;
+        }
+
+        //Prevent falling through the buttom
+        if(pY > 548) {//Ground level
+            pY = 548;
+            isOnGround = true;
+            verticalVelocity = 0;
+        }
+    }
+
     // Draw the player with appropriate image based on movement
     public void draw(Graphics g2d) {
         Image currentImage = standImage; // Default standing image
@@ -203,8 +114,11 @@ public class Player {
 
         System.out.println("PX: "+pX+ " PY: "+pY);
         
-        if(dir == 8) { // Player jump
-            pY -= pSpeed;
+        if(dir == 8 && isOnGround) { // Player jump
+            verticalVelocity =  jumpForce;
+            isOnGround = false;
+            isJumping = true; 
+            //pY -= pSpeed;
             // Prevents the player from moving out of the screen
             if (pY < 0)
                 pY = 0;
@@ -242,7 +156,31 @@ public class Player {
     }
 
     public void jump() {
-        // You can implement jump animation here
+        if(isOnGround) {
+            verticalVelocity =  jumpForce;
+            isOnGround = false;
+            isJumping =  true;
+        }
+    }
+
+
+    public void setIsOnGround(boolean onGround) {
+        this.isOnGround = onGround;
+        if (onGround) {
+            this.isJumping = false;
+        }
+    }
+
+    public void setVerticalVelocity(float velocity) {
+        this.verticalVelocity = velocity;
+    }
+
+    public boolean isFalling() {
+        return verticalVelocity > 0;
+    }
+
+    public void setY(int y) {
+        this.pY = y;
     }
 
     public boolean isOnPlayer(int x, int y) {
